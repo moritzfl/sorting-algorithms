27d2bab691deb542db8ab430a4ab6fc45b92bbc5
moritz <moritzf@gmail.com>
2018-04-05 18:28:55 +0200

    Add LatexUtil

---
+++ src/main/java/de/moritzf/sorting/gui/util/LatexUtil.java
@@ -0,0 +1,29 @@
+package de.moritzf.sorting.gui.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LatexUtil {
+
+    public static String normalizeTexExpression(String expression) {
+
+        return normalize(expression);
+    }
+
+    private static String normalize(String expression) {
+        if (expression != null) {
+
+            //insert linebreak between multiple math-environments
+            expression = expression.replaceAll(Pattern.quote("$") + "\\s*" + Pattern.quote("$"),
+                    Matcher.quoteReplacement("$\n\\\\\n$"));
+
+            //replace \newline command with \\ as jlatexmath can only handle \newline
+            expression = expression.replace("\\newline", "\\\\");
+        }
+
+        return expression;
+    }
+
+
+
+}
