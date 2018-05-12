faab5069f47f4e0c444d544c547f189bf897d00e
moritz <moritzf@gmail.com>
2018-05-12 16:17:23 +0200

    Bump jlatexmath-version to 1.0.7

--- src/main/java/de/moritzf/sorting/logic/util/IntArrayUtils.java
+++ src/main/java/de/moritzf/sorting/logic/util/IntArrayUtils.java
@@ -16,19 +16,4 @@ public class IntArrayUtils {
         System.arraycopy(src, 0, dest, 0, src.length);
         return dest;
     }
-
-    /**
-     * Returns a copy of the array with a new int-value added at its end.
-     *
-     * @param src        the source
-     * @param number2add the number2add
-     * @return the int[]
-     */
-    public static int[] copyAdd2IntArray(int[] src, int number2add) {
-        int[] dest = new int[src.length + 1];
-        System.arraycopy(src, 0, dest, 0, src.length);
-        dest[dest.length - 1] = number2add;
-        return dest;
-    }
-
 }
